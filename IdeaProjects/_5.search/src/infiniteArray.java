189d2e99e3e51ed54901a5e59c34284b66de1ced
iamtanishqsethi <tanishqsethi05@gmail.com>
2023-12-22 20:22:55 +0530

    searching algorithms added

---
+++ IdeaProjects/_5.search/src/infiniteArray.java
@@ -0,0 +1,33 @@
+public class infiniteArray {
+  /*
+to search in an infinite array , the only condition is we don't know the size of array
+so we can't use arr.length, to overcome that we move in chunks and
+ as we move forward we double the size of the chunk
+ */
+  public static int search(int[] arr,int target) {
+      int start = 0;
+      int end = 1;
+      while(target>arr[end]){
+          int temp=end+1;
+          end=end+(end-start+1)*2;//doubling the size of chunk as we move forward
+          start=temp;
+      }
+      while (start <= end) {
+          int mid = start + (end - start) / 2;
+          if (target == arr[mid]) {
+              return mid;
+          } else if (target > arr[mid]) {
+              start = mid + 1;
+          } else {
+              end = mid - 1;
+          }
+      }return -1;
+  }
+
+
+    public static void main(String[] args) {
+        int[] arr ={5,6,7,8,9,10};
+        int target = 10;
+        System.out.println(search(arr,target));
+    }
+}
---
+++ IdeaProjects/_5.search/src/mountainArray.java
@@ -0,0 +1,22 @@
+public class mountainArray {
+    public static int peakinmountainarray(int[] arr){
+        int start=0;
+        int end=arr.length-1;
+        while(start<end){
+            int mid=start+(end-start)/2;
+            if(arr[mid]>arr[mid+1]){
+                //we are asc part of mountain array
+                end=mid;
+            }else{
+                //we are in dsc part of mountain array
+                start=mid+1;
+            }
+        }//will keep running until start and end point at the same value ie the peak value
+        return start;
+    }
+
+    public static void main(String[] args) {
+        int[] arr={1,2,3,5,6,4,3,2};
+        System.out.println(peakinmountainarray(arr));
+    }
+}
