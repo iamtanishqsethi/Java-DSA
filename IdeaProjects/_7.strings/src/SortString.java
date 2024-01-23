39e78beccb49a72c35380324b302f6f6df0b6991
Tanishq Sethi <tanishqsethi05@gmail.com>
2024-01-23 23:06:14 +0530

    few string questions added

---
+++ IdeaProjects/_7.strings/src/SortString.java
@@ -0,0 +1,28 @@
+public class SortString {
+    //https://leetcode.com/problems/sorting-the-sentence/description/
+    public static void main(String[] args) {
+
+    }
+    public static String sortSentence(String s) {
+        String[] arr =s.split(" ");
+        String[] newarr =new String[arr.length ];
+        StringBuilder sb = new StringBuilder();
+
+        for(String word : arr){
+            char lastchar =word.charAt(word.length()-1);
+            if(Character.isDigit(lastchar)){//checking if last char is digit or not
+                int index = Character.getNumericValue(lastchar)-1;//converting it to int value
+                if(index >=0&&index<newarr.length){//avoiding index out of bound error
+                    newarr[index]=word.substring(0,word.length()-1);
+                }
+            }
+        }
+        for(String word : newarr){
+            if(word!=null){
+                sb.append(word);
+                sb.append(" ");
+            }
+        }
+        return sb.toString().trim();//removing white spaces
+    }
+}
