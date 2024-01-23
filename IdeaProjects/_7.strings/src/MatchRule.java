01d46f51596af680de037e26e530182801214d35
Tanishq Sethi <148245926+iamtanishqsethi@users.noreply.github.com>
2024-01-23 23:10:50 +0530

    few string questions added

---
+++ IdeaProjects/_7.strings/src/MatchRule.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+public class MatchRule {
+    //https://leetcode.com/problems/count-items-matching-a-rule/description/
+    public static int countMatches(List<List<String>> items, String ruleKey, String ruleValue){
+        int res = 0;
+        for(int i=0;i<items.size();i++){
+            if(ruleKey.equals("type") &&items.get(i).get(0).equals(ruleValue)){
+                res++;
+            }if(ruleKey.equals("color") &&items.get(i).get(1).equals(ruleValue)){
+                res++;
+            }if(ruleKey.equals("name") &&items.get(i).get(2).equals(ruleValue)){
+                res++;
+            }
+        }return res;
+    }
+}
