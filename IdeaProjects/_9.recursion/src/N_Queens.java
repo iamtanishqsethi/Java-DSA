386867a76877bb0c2508f9e85c84a395e10aeb2a
Tanishq Sethi <148245926+iamtanishqsethi@users.noreply.github.com>
2024-02-20 18:21:49 +0530

    Backtracking questions
    added some imp questions based on backtracking - n queens , n knights and sudoku solver
    (not currently optimized)

---
+++ IdeaProjects/_9.recursion/src/N_Queens.java
@@ -0,0 +1,60 @@
+public class N_Queens {
+    public static void main(String[] args) {
+        int n =4;
+        boolean[][] board = new boolean[n][n];
+        System.out.println(queens(board,0));
+;    }
+    static int queens(boolean[][] board,int row){
+        if(row==board.length){
+            display(board);
+            System.out.println();
+            return 1;
+        }
+        int count =0;
+        //placing the queen and checking for every row and col
+        for(int col=0;col<board.length;col++){
+            //place the queens if t is safe
+            if(isSafe(board,row,col)){
+                board[row][col]=true;
+                count+=queens(board,row+1);
+                board[row][col]=false;
+            }
+        }
+        return count;
+    }
+    static void display(boolean[][] board){
+        for(boolean[] row:board){
+            for(boolean element:row){
+                if(element){
+                    System.out.print("Q ");
+                }else{
+                    System.out.print("X ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    static boolean isSafe(boolean[][] board,int row,int col){
+        //vertical row
+        for(int i =0;i<row;i++){
+            if(board[i][col]){
+                return false;
+            }
+        }
+        //left diagonal
+        int maxleft = Math.min(row,col);
+        for(int i =1;i<=maxleft;i++){
+            if(board[row-i][col-i]){
+                return false;
+            }
+        }
+        //right diagonal
+        int maxright = Math.min(row,board.length-col-1);
+        for(int i =1;i<=maxright;i++){
+            if(board[row-i][col+i]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
