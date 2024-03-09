a386e30d6441c1ee03b17c4d46c7bdaaf50a4c6e
Tanishq Sethi <148245926+iamtanishqsethi@users.noreply.github.com>
2024-03-10 00:38:52 +0530

    OOP 2

--- IdeaProjects/OOP/src/Wrapper.java
+++ IdeaProjects/OOP/src/Wrapper.java
@@ -1,5 +1,19 @@
 import java.rmi.StubNotFoundException;
 
+class A {
+    final int num = 10;//initialization is important
+    String name;
+    public A(String name){
+        this.name = name;
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.println("Object is destroyed");
+    }
+}
+
+
 public class Wrapper {
     public static void main(String[] args) {
         int a = 10;//primitive
@@ -56,15 +70,4 @@ public class Wrapper {
 //        System.out.println("b="+b);
     }
 }
-class A {
-    final int num = 10;//initialization is important
-    String name;
-    public A(String name){
-        this.name = name;
-    }
 
-    @Override
-    protected void finalize() throws Throwable {
-        System.out.println("Object is destroyed");
-    }
-}
