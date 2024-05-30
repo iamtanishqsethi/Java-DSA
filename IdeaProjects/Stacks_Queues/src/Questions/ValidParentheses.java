29cd58a78dbead6c9300531621741f709016f5f6
Tanishq Sethi <tanishqsethi05@gmail.com>
2024-05-30 18:41:17 +0530

    stacks and queue questions video completed

---
+++ IdeaProjects/Stacks_Queues/src/Questions/ValidParentheses.java
@@ -0,0 +1,31 @@
+package Questions;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(char ch:s.toCharArray()){
+            if(ch=='('||ch=='{'||ch=='['){
+                stack.push(ch);
+            }else{
+                if(ch==')'){
+                    if(stack.isEmpty()||stack.pop()!='('){
+                        return false;
+                    }
+                }
+                if(ch=='}'){
+                    if(stack.isEmpty()||stack.pop()!='{'){
+                        return false;
+                    }
+                }
+                if(ch==']'){
+                    if(stack.isEmpty()||stack.pop()!='['){
+                        return false;
+                    }
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
