37183a341340564791cf928b4143ed7e4fe02cac
Tanishq Sethi <tanishqsethi05@gmail.com>
2024-07-18 15:52:12 +0530

    big numbers

---
+++ IdeaProjects/LargeNumbers/src/Main.java
@@ -0,0 +1,13 @@
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) {
+        int a=30;
+        int b=67;
+        BigInteger A= BigInteger.valueOf(33);
+        BigInteger B= BigInteger.valueOf(575869879);
+
+    //https://docs.oracle.com/javase/8/docs/api/java/math/BigInteger.html
+        //documentation for big integer
+    }
+}
