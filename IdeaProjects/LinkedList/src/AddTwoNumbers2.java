4a4ac70114cb3e23a76895e5ff0ba60ecc7d0cd5
Tanishq Sethi <tanishqsethi05@gmail.com>
2024-08-07 07:22:25 +0530

    fixed add 2 numpers 2 code

--- IdeaProjects/LinkedList/src/AddTwoNumbers2.java
+++ IdeaProjects/LinkedList/src/AddTwoNumbers2.java
@@ -1,27 +1,35 @@
 import java.util.Stack;
-//wrong code
-//TODO:fix code 
+//optimize code
+//https://leetcode.com/problems/add-two-numbers-ii/description/
 public class AddTwoNumbers2 {
     //hint taken - store  in stack
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        Stack<Integer> stack= new Stack<>();
+        Stack<Integer> stack1= new Stack<>();
+        Stack<Integer> stack2= new Stack<>();
+        Stack<Integer> stack3=new Stack<>();
+        while(l1!=null){
+            stack1.push(l1.val);
+            l1=l1.next;
+        }
+        while(l2!=null){
+            stack2.push(l2.val);
+            l2=l2.next;
+        }
         int carry=0;
-        while(l1!=null||l2!=null){
-            int value1=(l1!=null)?l1.val:0;
-            int value2=(l2!=null)?l2.val:0;
-            int sum=value1+value2+carry;
-            carry=sum/10;
-            stack.push(sum%10);
-            if (l1 != null) l1 = l1.next;
-            if (l2 != null) l2 = l2.next;
-        }
-        if(carry>0){
-            stack.push(carry);
-        }
         ListNode dummyHead=new ListNode(0);
         ListNode current=dummyHead;
-        while(!stack.isEmpty()){
-            current.next=new ListNode(stack.pop());
+        while(!stack1.isEmpty()||!stack2.isEmpty()){
+            int value1=(!stack1.isEmpty())?stack1.pop():0;
+            int value2=(!stack2.isEmpty())?stack2.pop():0;
+            int sum=value1+value2+carry;
+            carry=sum/10;
+            stack3.push(sum%10);
+        }
+        if(carry>0){
+            stack3.push(carry);
+        }
+        while (!stack3.isEmpty()){
+            current.next=new ListNode(stack3.pop());
             current=current.next;
         }
         return dummyHead.next;
